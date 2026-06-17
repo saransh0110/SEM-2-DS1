aa3da3da1c6d5e53f805e3d8adee62424ff7b9ac
saransh0110 <off.sg17@gmail.com>
2026-06-17 20:38:31 +0530

    Add ST1 folder

---
+++ ST1/script.js
@@ -0,0 +1,11 @@
+function calculateSum() {
+
+    var n = document.getElementById("num").value;
+    var sum = 0;
+
+    for (var i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    document.getElementById("result").innerHTML = "Sum =" + sum;
+}
